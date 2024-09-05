f87b327fbeb96abbd3609c55167a229f54f003f2
goelaine <elainego@andrew.cmu.edu>
2024-09-05 17:46:07 -0400

    Revert "trying to write new test cases to cover categories/tools.js"
    This reverts commit 08c8643f3c5692bf03d8dac247e649063805c9d8.
    
    revert test cases: did not work

--- test/ElaineTests.js
+++
@@ -1,7 +0,0 @@
-
-test('CategoryTools.init should call topicSelect.init with updateDropdownOptions', () => {
-    const updateDropdownOptions = jest.fn();
-    const initSpy = jest.spyOn(topicSelect, 'init');
-    CategoryTools.init();
-    expect(initSpy).toHaveBeenCalledWith(updateDropdownOptions);
-});
